401052f77dc2c90618501d4825444f0ad34d08b5
Gab De Jesus <gabrieldejesus2112@gmail.com>
2017-05-24 18:48:56 +0900

    Fixed BlinkingLed

--- BlinkingLed/BlinkingLed.ino
+++ BlinkingLed/BlinkingLed.ino
@@ -1,18 +1,19 @@
 /*
  * Code written by Gabriel De Jesus / quillot.github.com
  * 5/24/2017 
+ * Makes an LED blink on and off every second
  */
- 
+int ledPin = 10;
+
 void setup() {
   // The code here will run once and setup the arduino
-  int ledPin = 10;  // Initialize pin 10 so it can activate the led
+  pinMode(ledPin, OUTPUT);  // Initialize pin 10 so it can output power
 }
 
 void loop() {
   // The code here will run as long as arduino is on
-  // The led will blink on and off every second
-  pinMode(ledPin, HIGH);  // Output HIGH on pin 10 to activate the led
-  delay(100);  // Wait a second before deactivating the led
-  pinMode(ledPin, LOW);  // Output LOW on pin 10 to deactivate the led
-  delay(100); // Wait a second before activating the led
+  digitalWrite(ledPin, HIGH);  // Output HIGH on pin 10 to activate the led
+  delay(1000);  // Wait a second before deactivating the led
+  digitalWrite(ledPin, LOW);  // Output LOW on pin 10 to deactivate the led
+  delay(1000); // Wait a second before activating the led
 }
