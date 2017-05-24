37f7d4d54bbf6bdfc3ba58dd454d20d678ca0751
Gab De Jesus <gabrieldejesus2112@gmail.com>
2017-05-24 18:35:22 +0900

    Added blinking led program

---
+++ led/led.ino
@@ -0,0 +1,18 @@
+/*
+ * Code written by Gabriel De Jesus / quillot.github.com
+ * 5/24/2017 
+ */
+ 
+void setup() {
+  // The code here will run once and setup the arduino
+  int ledPin = 10;  // Initialize pin 10 so it can activate the led
+}
+
+void loop() {
+  // The code here will run as long as arduino is on
+  // The led will blink on and off every second
+  pinMode(ledPin, HIGH);  // Output HIGH on pin 10 to activate the led
+  delay(100);  // Wait a second before deactivating the led
+  pinMode(ledPin, LOW);  // Output LOW on pin 10 to deactivate the led
+  delay(100); // Wait a second before activating the led
+}
